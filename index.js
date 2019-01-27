62000a326679295b0e8d83c73efce3ef9566830c
febanda01 <febanda91@gmail.com>
2019-01-27 17:23:42 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function BoardMember(name, homeState, training){
+    this.name = name 
+    this.homeState = homeState 
+    this.training = training 
+    this.veto = function(){
+        return "No, I must disagree"
+    }
+    this.approve = function(){
+        return "You can do that!"
+    }
+    this.doCharity = function(){
+        return "I like to help people"
+    }
+    this.releasePressStatement = function(){
+        return "You will see great things from Scuber"
+    }
+    this.sayHi = function(){
+        `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`
+    }
+}
+
+
+
+
+
+function User (name, email){
+    this.name = name;
+    this.email = email;
+    this.sayHello = function(){
+      console.log(`Hello everybody, my name is ${this.name}!`);
+    }
+  }
